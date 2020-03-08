6ab1117c2e386fd959e5799595b30c69dc85e7ed
Agustin Burset <agustin.burset@globant.com>
2020-03-08 16:29:07 -0300

    Add books state and update methods, provide them via BooksContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { BooksContext } from './contexts/BooksContext';
+import { Book } from './models/Book';
 
 export const App = () => {
+  const [books, setBooks] = useState([
+    new Book('Foundation', 'Isaac Asimov', uuid()),
+    new Book('The Martian', 'Andy Weir', uuid()),
+  ]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, new Book(title, author, uuid())]);
+  }
+  const removeBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+  }
+
   return (
     <div className="App">
+      <BooksContext.Provider value={[books, addBook, removeBook]}>
+      </BooksContext.Provider>
     </div>
   );
 }
