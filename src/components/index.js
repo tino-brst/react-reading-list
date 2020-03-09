75c75f38b93e7010bdb6a05b738d4feb408abfd2
Agustin Burset <agustin.burset@globant.com>
2020-03-09 10:21:18 -0300

    Add index.js files for cleaner imports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './BookList';
+export * from './BookListTile';
+export * from './Navbar';
+export * from './NewBookForm';
+export * from './ThemeSelect';
---
+++ src/contexts/index.js
@@ -0,0 +1,2 @@
+export * from './BooksContext';
+export * from './ThemeContext';
