572ca9cea696838ca1fa9dcbdc41cdfa1f47b0a6
Agustin Burset <agustin.burset@globant.com>
2020-03-09 10:14:33 -0300

    Add ThemeSelect component to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
 import { BooksContext } from '../contexts/BooksContext';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { ThemeSelect } from './ThemeSelect';
 import { NewBookForm } from './NewBookForm';
 
 export const Navbar = () => {
   const { books } = useContext(BooksContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <nav className="Navbar">
+      <ThemeSelect value={theme} onChange={(event) => setTheme(event.target.value)} />
       <h1>My Reading List</h1>
       <h3>{books.length} books to go</h3>
       <NewBookForm />
