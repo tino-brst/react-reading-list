ff2bc668e897e77bc59b0db9fd8f15e90417ba0f
Agustin Burset <agustin.burset@globant.com>
2020-03-18 09:43:42 -0300

    Convert reducers to Typescript, add uuid type definitions, fix errors

--- src/reducers/books.ts
+++ src/reducers/books.ts
@@ -1,8 +1,9 @@
 import { v4 as uuid } from 'uuid'
 import { BooksActionType } from '../actions'
 import { Book } from '../models'
+import { Action } from '../actions/Action'
 
-export function booksReducer(books, action) {
+export function booksReducer(books: Array<Book>, action: Action) {
   switch (action.type) {
     case BooksActionType.add:
       const { title, author } = action.payload
